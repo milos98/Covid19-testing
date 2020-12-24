9e70b0596d54afc5e34ed5585d4ee6d4196869d3
Miloš Janković <mjankovic180@gmail.com>
2020-12-24 05:05:57 +0100

    User registration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 // Register
 router.post('/register', (req, res) => {
-    res.send('REGISTER');
+    let newUser = new User({
+        name: {
+            first: req.body.firstName,
+            last: req.body.lastName
+        },
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+        gender: req.body.gender,
+        userType: (req.body.userType) ? req.body.userType : "standard",
+        lastSeen: null,
+        testId: [],
+        status: {
+            status: "healty",
+            dateFrom: null,
+            dateTo: null,
+            dateDateOfLastSelfTest: null
+        }
+    });
+
+    User.addUser(newUser, (error, user) => {
+        if(error){
+            res.json({success: false, message: "Registration failed!"})
+        } else {
+            res.json({success: true, message: "User registered!"})
+        }
+    })
 });
 
 // Authenticate
