dd452e8992821f4cb6749e2769968cd5b93a3eb1
Miloš Janković <mjankovic180@gmail.com>
2020-12-24 04:09:55 +0100

    Mongo Atlas DB connection

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
+// Mongo Atlas BD connection
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(config.database);
+
+// On Connect
+mongoose.connection.on('connected', () => {
+    console.log('Connected to DB');
+});
+
+// On error
+mongoose.connection.on('error', (error) => {
+    console.log('Error connecting to DB: ' +error);
+});
+
 const app = express();
 
 const users = require('./routes/users');
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    database: 'mongodb+srv://milos_jankovic:novaSifra.1975@covid19-cluster.i7srr.mongodb.net/Covid19?retryWrites=true&w=majority'
+}
